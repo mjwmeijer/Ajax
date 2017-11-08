f35add5564649b638a80cc5799ab7f27ba7661a9
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-08 09:46:23 +0100

    test load method jQuery css selector.

--- browser258.js
+++ browser258.js
@@ -1,6 +1,7 @@
 $(document).ready(function (){
   $('#btnLoad').on('click',function(){
-$('#divResult').load('blz258laad.html')
+$('#divResult').load('blz258laad.html');
+$('#divResult2').load('blz258laad.html #content');
 });
 
 })
