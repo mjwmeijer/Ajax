f8e61edde753d90dd4e95545e2be37c83de63168
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-08 10:01:32 +0100

    test load method jQuery with callback function.

--- browser258.js
+++ browser258.js
@@ -1,7 +1,12 @@
 $(document).ready(function (){
   $('#btnLoad').on('click',function(){
 $('#divResult').load('blz258laad.html');
-$('#divResult2').load('blz258laad.html #content');
+$('#divResult2').load('blz258laad.html #content',
+function(){
+  $('#divAlert').html('Laden geslaagd');
+});
 });
 
 })
+
+//de load() method accepteert ook een data argument, waarmee de te laden pagina iets kan doen
